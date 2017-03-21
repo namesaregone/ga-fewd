cb04098a35059f762cbcd8c59897aaac96f325d3
namesaregone <namesaregone@gmail.com>
2017-03-21 18:02:30 -0400

    Homework, classwork, all work.

---
+++ d09/homework/travelling-dwarf/bonus-solution.js
@@ -0,0 +1,5 @@
+// `g.at(x, y)` 
+// will tell you if there is an obstacle at the coordinate you specify...
+//   - `d.orientation` will tell you your current orientation 
+// (ie, 'left', 'right', 'down', 'up')
+
---
+++ d09/homework/travelling-dwarf/solution.js
@@ -0,0 +1,43 @@
+console.log("WRITE YOUR SOLUTION IN HERE!");
+
+for(i = 0; i < 4; i++){
+d.move();
+}
+
+d.orient("down")
+
+for(i = 0; i < 16; i++){
+d.move();
+}
+
+d.orient("right")
+
+for(i = 0; i < 6; i++){
+d.move();
+}
+
+d.orient("up")
+
+for(i = 0; i < 11; i++){
+d.move();
+}
+
+d.orient("right")
+
+for(i = 0; i < 6; i++){
+d.move();
+}
+
+d.orient("down")
+
+for(i = 0; i < 9; i++){
+d.move();
+}
+
+d.orient("right")
+
+for(i = 0; i < 2; i++){
+d.move();
+}
+
+
